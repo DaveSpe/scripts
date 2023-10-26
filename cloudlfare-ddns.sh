03a8ae93b0df7b1fa47525aacaf45183177579ef
Dave S <65930418+DaveSpe@users.noreply.github.com>
2023-10-26 07:50:37 -0600

    Create cloudlfare-ddns.sh

---
+++ cloudlfare-ddns.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Script uses the Cloudflare API, create a token in your dns zone with: Zone.Zone Settings, Zone.Zone, Zone.DNS permissions.
+# Cloudflare specific variables
+# indentifier value can be obtained by running the following shell command using your api token:
+# curl --request GET --url https://api.cloudflare.com/client/v4/zones/<zone_identifier>/dns_records/ -H "Authorization: Bearer <api_token>" -H "Content-Type:application/json" | jq .result
+api_token=<api_token>
+zone_identifier=<zone_identifier>
+identifier=<identifier>
+domain_name="<your-domain>" # ex. "*.my-domain.com"
+
+# fetch current public IP
+my_ip=$(curl -s ifconfig.me)
+# echo "my_ip: "$my_ip # debug
+# fetch public IP registered with Cloudflare
+cf_ip=$(curl -s --request GET --url https://api.cloudflare.com/client/v4/zones/${zone_identifier}/dns_records/${identifier} -H "Authorization: Bearer "${api_token} -H "Content-Type:application/json" | jq .result.content | tr -d '"')
+# echo "cf_ip: "$cf_ip # debug
+
+if [[ $my_ip == $cf_ip ]] 
+then
+    echo "Public IP matches Cloudflare IP"
+else
+    echo "Updating IP within Cloudflare from: $cf_ip, to: $my_ip"
+    # Update and register result of update
+    result=$(curl -s --request PUT --url https://api.cloudflare.com/client/v4/zones/${zone_identifier}/dns_records/${identifier} -H "Content-Type: application/json" -H "Authorization: Bearer ${api_token}" -d '{"content": "'${my_ip}'", "name": "'${domain_name}'", "proxied": true, "type": "A", "tags": [], "ttl": 1}' | jq .success)
+    # echo "result: "$result # debug
+    if [[ $result == true ]]
+    then
+        echo "DNS updated successfully"
+    else
+        echo "Failed to update DNS with new IP"
+    fi
+fi
